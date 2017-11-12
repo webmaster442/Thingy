d54306d1843c2c5b33fad4cb19243633412210e3
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-11-12 20:37:30 +0100

    Tile

---
+++ Thingy/Controls/Tile.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Thingy.Controls
+{
+    /// <summary>
+    /// Interaction logic for Tile.xaml
+    /// </summary>
+    public partial class Tile : UserControl
+    {
+        public static readonly DependencyProperty TileTextProperty =
+            DependencyProperty.Register("TileText", typeof(string), typeof(Tile), new PropertyMetadata("Tile Text"));
+
+        public static readonly DependencyProperty TileImageProperty =
+            DependencyProperty.Register("TileImage", typeof(ImageSource), typeof(Tile));
+
+        public Tile()
+        {
+            InitializeComponent();
+        }
+    }
+}
