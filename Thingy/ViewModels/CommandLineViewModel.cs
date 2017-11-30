0e8562655de94f7a37daf14e2739f59c88980955
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-11-30 20:53:59 +0100

    Command line module complete

--- Thingy/ViewModels/CommandLineViewModel.cs
+++ Thingy/ViewModels/CommandLineViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using AppLib.MVVM;
 using CmdHost;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Thingy.Views;
 
