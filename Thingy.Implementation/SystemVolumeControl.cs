96063872d2c7cd3a00a1c9804ea45951b78dee4c
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-12-01 20:55:49 +0100

    System volume control implementation

---
+++ Thingy.Implementation/SystemVolumeControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Thingy.Implementation
+{
+    public static class SystemVolumeControl
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        public static void Mute()
+        {
+            SendMessageW(IntPtr.Zero, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        public static void VolumeDown()
+        {
+            SendMessageW(IntPtr.Zero, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        public static void VolumeUp()
+        {
+            SendMessageW(IntPtr.Zero, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+    }
+}
