3fe7f46afdff65a29e1533adba94518f20e42e4a
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-12-09 10:43:20 +0100

    Launcher program class added

---
+++ Thingy.Db/Entity/LauncherProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.Db.Entity
+{
+    public class LauncherProgram
+    {
+    }
+}
