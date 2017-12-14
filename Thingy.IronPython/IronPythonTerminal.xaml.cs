7cd273f75845be77dff0d8b992233cdc15cd2a44
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-12-14 22:49:03 +0100

    Iron Python project skeleton

---
+++ Thingy.IronPython/IronPythonTerminal.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Thingy.IronPython
+{
+    /// <summary>
+    /// Interaction logic for IronPythonTerminal.xaml
+    /// </summary>
+    public partial class IronPythonTerminal : UserControl
+    {
+        public IronPythonTerminal()
+        {
+            InitializeComponent();
+        }
+    }
+}
