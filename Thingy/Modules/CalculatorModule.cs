e86364d5306f96749b5137e1490363894dfc8054
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-02 14:32:33 +0100

    Calculator Skeleton

---
+++ Thingy/Modules/CalculatorModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Thingy.Infrastructure;
+
+namespace Thingy.Modules
+{
+    public class CalculatorModule : ModuleBase
+    {
+        public override string ModuleName
+        {
+            get { return "Calculator"; }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return new BitmapImage(new Uri("pack://application:,,,/Thingy.Resources;component/Icons/icons8-calculator-96.png")); }
+        }
+
+        public override string Category
+        {
+            get { return ModuleCategories.Calculators; }
+        }
+
+        public override UserControl RunModule()
+        {
+            return new Views.Calculator();
+        }
+    }
+}
