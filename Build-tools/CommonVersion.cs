f942e71567b0db19c47e051844190d080de6ce55
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-19 18:36:25 +0100

    Windows power controls implemented

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.1.19.433")]
-[assembly: AssemblyFileVersion("2018.1.19.433")]
+[assembly: AssemblyVersion("2018.1.19.435")]
+[assembly: AssemblyFileVersion("2018.1.19.435")]
--- Thingy/Views/Statusbar/WindowsPower.xaml.cs
+++ Thingy/Views/Statusbar/WindowsPower.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppLib.Common.PInvoke;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Thingy.Views.Statusbar
 {
@@ -23,6 +12,36 @@ namespace Thingy.Views.Statusbar
         public WindowsPower()
         {
             InitializeComponent();
+        }
+
+        private void Lock_Click(object sender, RoutedEventArgs e)
+        {
+            User32.LockWorkStation();
+        }
+
+        private void Sleep_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Suspend, true, true);
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            User32.ExitWindowsEx(ExitWindows.EWX_LOGOFF, ShutdownReason.FlagPlanned);
+        }
+
+        private void Hybernate_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Application.SetSuspendState(System.Windows.Forms.PowerState.Hibernate, true, true);
+        }
+
+        private void Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            User32.ExitWindowsEx(ExitWindows.EWX_POWEROFF, ShutdownReason.FlagPlanned);
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            User32.ExitWindowsEx(ExitWindows.EWX_REBOOT, ShutdownReason.FlagPlanned);
         }
     }
 }
