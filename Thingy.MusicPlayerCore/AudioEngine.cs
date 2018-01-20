149fccfbb8173be72b534e8f26fc29fadb98604d
webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-20 12:09:45 +0100

    Audio Engine Basic layout

---
+++ Thingy.MusicPlayerCore/AudioEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.MusicPlayerCore
+{
+    public sealed class AudioEngine : IAudioEngine, IDisposable
+    {
+        public AudioEngine()
+        {
+            Log = new AudioEngineLog();
+        }
+        
+        public AudioEngineLog Log { get; }
+
+        public bool Is64BitProcess
+        {
+            get { return IntPtr.Size == 8; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Thingy.MusicPlayerCore/IAudioEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.MusicPlayerCore
+{
+    public interface IAudioEngine
+    {
+        AudioEngineLog Log { get; }
+        bool Is64BitProcess { get; }
+    }
+}
