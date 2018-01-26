85f626f4d835b313c93f91f355e10212d5d3840a
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-26 22:31:43 +0100

    Styles fix

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.1.26.546")]
-[assembly: AssemblyFileVersion("2018.1.26.546")]
+[assembly: AssemblyVersion("2018.1.26.547")]
+[assembly: AssemblyFileVersion("2018.1.26.547")]
