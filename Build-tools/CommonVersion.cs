4c06c8398c0f4be0b24ffe4f994f708bde48fa1c
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-27 09:29:19 +0100

    Borderless button style fix

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.1.26.547")]
-[assembly: AssemblyFileVersion("2018.1.26.547")]
+[assembly: AssemblyVersion("2018.1.27.551")]
+[assembly: AssemblyFileVersion("2018.1.27.551")]
