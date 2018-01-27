efaa7615c5be19de4b53b7e6a7a2fdb4a36966f0
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-27 09:40:27 +0100

    Fonts

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.1.27.551")]
-[assembly: AssemblyFileVersion("2018.1.27.551")]
+[assembly: AssemblyVersion("2018.1.27.555")]
+[assembly: AssemblyFileVersion("2018.1.27.555")]
