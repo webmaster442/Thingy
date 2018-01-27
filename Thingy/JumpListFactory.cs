602e8f59d5429571d450938020e103ec7fe62041
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-27 10:11:12 +0100

    Jumplist Factory

---
+++ Thingy/JumpListFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Shell;
+
+namespace Thingy
+{
+    internal static class JumpListFactory
+    {
+        private static IEnumerable<JumpTask> Tasks
+        {
+            get
+            {
+                yield return new JumpTask()
+                {
+                    Title = "Restart",
+                    Arguments = "/restart",
+                    Description = "Restarts the application",
+                    CustomCategory = "Actions",
+                    IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
+                    ApplicationPath = Assembly.GetEntryAssembly().CodeBase
+                };
+                yield return new JumpTask()
+                {
+                    Title = "Exit",
+                    Arguments = "/exit",
+                    Description = "Exit from application",
+                    CustomCategory = "Actions",
+                    IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
+                    ApplicationPath = Assembly.GetEntryAssembly().CodeBase
+                };
+            }
+        }
+
+        public static void CreateJumplist()
+        {
+            var appmenu = new JumpList();
+            appmenu.JumpItems.AddRange(Tasks);
+            appmenu.ShowFrequentCategory = false;
+            appmenu.ShowRecentCategory = false;
+            JumpList.SetJumpList(Application.Current, appmenu);
+        }
+    }
+}
