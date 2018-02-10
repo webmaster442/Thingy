fc0e5d2067a1c9949b7a08458ff24c3cae26d140
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-10 12:37:16 +0100

    FFMpeg Gui module work

---
+++ Thingy/Modules/FFMpegGuiModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Thingy.Infrastructure;
+
+namespace Thingy.Modules
+{
+    public class FFMpegGuiModule : ModuleBase
+    {
+        public override string ModuleName
+        {
+            get { return "FFMpegGui"; }
+        }
+
+        public override ImageSource Icon => throw new NotImplementedException();
+
+        public override string Category => throw new NotImplementedException();
+
+        public override UserControl RunModule()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
