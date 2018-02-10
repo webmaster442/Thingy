309d43a403c03ebbd29eef39919aa7eaa33a48cf
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-10 12:09:22 +0100

    FFMpegGui skeleton

---
+++ Thingy.UiBuilder/Controls/SliderControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Thingy.FFMpegGui.Controls
+{
+    public class SliderControl : BaseControl
+    {
+        private Slider _slider;
+
+        public SliderControl(string name): base(name)
+        {
+            _slider = new Slider();
+        }
+
+        public double Minimum
+        {
+            get { return _slider.Minimum; }
+            set { _slider.Minimum = value; }
+        }
+
+        public double Maximum
+        {
+            get { return _slider.Maximum; }
+            set { _slider.Maximum = value; }
+        }
+
+        public double Value
+        {
+            get { return _slider.Value; }
+            set { _slider.Value = value; }
+        }
+
+        public override FrameworkElement Visual
+        {
+            get { return _slider; }
+        }
+    }
+}
---
+++ Thingy.UiBuilder/Preset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Thingy.FFMpegGui.Controls;
+
+namespace Thingy.FFMpegGui
+{
+    public abstract class Preset
+    {
+        public List<BaseControl> Controls { get; }
+
+        public Preset()
+        {
+            Controls = new List<BaseControl>();
+        }
+    }
+}
