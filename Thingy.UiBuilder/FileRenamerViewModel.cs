6c03ff30d9b1691154c450eb0a84c497ad2d50be
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-12 23:03:51 +0100

    File renamer control work in progress

---
+++ Thingy.UiBuilder/FileRenamerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AppLib.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.FFMpegGui
+{
+    public class FileRenamerViewModel: ViewModel
+    {
+        private string _filenamePattern;
+        private string _extensionPattern;
+
+        public string FilenamePattern
+        {
+            get { return _filenamePattern; }
+            set { SetValue(ref _filenamePattern, value); }
+        }
+
+        public string ExtensionPattern
+        {
+            get { return _extensionPattern; }
+            set { SetValue(ref _extensionPattern, value); }
+        }
+    }
+}
