844fe27a1b1b1709acc695e4c33aa7b9fb8724c8
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-14 21:35:51 +0100

    File renamer tool

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.2.12.774")]
-[assembly: AssemblyFileVersion("2018.2.12.774")]
+[assembly: AssemblyVersion("2018.2.13.775")]
+[assembly: AssemblyFileVersion("2018.2.13.775")]
--- Thingy.UiBuilder/FileRenamer.xaml.cs
+++ Thingy.UiBuilder/FileRenamer.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Thingy.FFMpegGui
 {
@@ -23,6 +12,46 @@ namespace Thingy.FFMpegGui
         public FileRenamer()
         {
             InitializeComponent();
+        }
+
+        public IEnumerable<string> InputFiles
+        {
+            get { return (IEnumerable<string>)GetValue(InputFilesProperty); }
+            set { SetValue(InputFilesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InputFiles.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InputFilesProperty =
+            DependencyProperty.Register("InputFiles", typeof(IEnumerable<string>), typeof(FileRenamer), new PropertyMetadata(null, InputFilesChanged));
+
+        private static void InputFilesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as FileRenamer;
+            var model = ctrl.DataContext as FileRenamerViewModel;
+            if (model != null)
+            {
+                model.InputFiles = e.NewValue as IEnumerable<string>;
+            }
+        }
+
+        public string OutputFolder
+        {
+            get { return (string)GetValue(OutputFolderProperty); }
+            set { SetValue(OutputFolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OutputFolder This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OutputFolderProperty =
+            DependencyProperty.Register("OutputFolder", typeof(string), typeof(FileRenamer), new PropertyMetadata(null, OutputFolderChanged));
+
+        private static void OutputFolderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as FileRenamer;
+            var model = ctrl.DataContext as FileRenamerViewModel;
+            if (model != null)
+            {
+                model.OutputFolder = e.NewValue as string;
+            }
         }
     }
 }
