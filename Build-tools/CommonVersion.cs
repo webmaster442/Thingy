33ac4d6349a168655574a0bf0310837545670588
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-21 20:05:54 +0100

    Audio 2 alac preset

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.2.21.827")]
-[assembly: AssemblyFileVersion("2018.2.21.827")]
+[assembly: AssemblyVersion("2018.2.21.828")]
+[assembly: AssemblyFileVersion("2018.2.21.828")]
---
+++ Thingy.UiBuilder/Presets/AudioToALAC.cs
@@ -0,0 +1,20 @@
+﻿namespace Thingy.FFMpegGui.Presets
+{
+    public class AudioToALAC : Preset
+    {
+        public override string Name
+        {
+            get { return "Audio to ALAC"; }
+        }
+
+        public override string Description
+        {
+            get { return "Convert audio to Apple losless format. To be fully compatible choose an m4a extension"; }
+        }
+
+        public override string CommandLine
+        {
+            get { return $"ffmpeg.exe -i \"{InputFile}\" -vn -acodec alac \"{OutputFile}\""; }
+        }
+    }
+}
