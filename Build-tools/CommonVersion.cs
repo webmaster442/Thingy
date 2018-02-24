b7bb328672d9b3421efb385c155c45f9f917a441
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-24 11:56:16 +0100

    New notes moduel icons

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.2.23.873")]
-[assembly: AssemblyFileVersion("2018.2.23.873")]
+[assembly: AssemblyVersion("2018.2.24.874")]
+[assembly: AssemblyFileVersion("2018.2.24.874")]
