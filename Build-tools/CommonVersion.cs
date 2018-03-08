15abe3c2bf69bdd20709a2289443fba459def867
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-03-08 21:38:59 +0100

    Minor changes to query editor

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.3.8.981")]
-[assembly: AssemblyFileVersion("2018.3.8.981")]
+[assembly: AssemblyVersion("2018.3.8.982")]
+[assembly: AssemblyFileVersion("2018.3.8.982")]
