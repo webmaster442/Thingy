0f0535d017789b75a5260f6d084e72da2f96c92e
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-03-15 20:36:36 +0100

    Nuget package update

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.3.12.1035")]
-[assembly: AssemblyFileVersion("2018.3.12.1035")]
+[assembly: AssemblyVersion("2018.3.15.1036")]
+[assembly: AssemblyFileVersion("2018.3.15.1036")]
