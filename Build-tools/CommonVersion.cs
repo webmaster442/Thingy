6be650b03ee06b29fc1c1d8f3a808def1fecb2ae
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-10 21:03:13 +0200

    Audio player works

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.4.10.1187")]
-[assembly: AssemblyFileVersion("2018.4.10.1187")]
+[assembly: AssemblyVersion("2018.4.10.1198")]
+[assembly: AssemblyFileVersion("2018.4.10.1198")]
--- Thingy.XAML/Converters/AudioDeviceIndexToListIndexConverter.cs
+++ Thingy.XAML/Converters/AudioDeviceIndexToListIndexConverter.cs
@@ -6,16 +6,18 @@ using System.Windows.Markup;
 
 namespace Thingy.XAML.Converters
 {
-    public class AudioDeviceIndexToListIndexConverter: MarkupExtension, IValueConverter
+    public class AudioDeviceIndexToListIndexConverter: MarkupExtension, IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            IDictionary<string, int> devices = parameter as IDictionary<string, int>;
+            IDictionary<string, int> devices = values[0] as IDictionary<string, int>;
 
             if (devices == null) return Binding.DoNothing;
 
-            int index = System.Convert.ToInt32(value);
+            int index = System.Convert.ToInt32(values[1]);
+
             int currentindex = 0;
+
             foreach (var keyvalue in devices)
             {
                 if (keyvalue.Value == index)
@@ -27,9 +29,9 @@ namespace Thingy.XAML.Converters
             return -1;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            return -1;
+            return null;
         }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
