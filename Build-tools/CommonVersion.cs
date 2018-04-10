9614fa10a6cd8f59b7a0ca245fe7a4db9b5c1721
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-10 20:45:08 +0200

    Modules moved to modules folder

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.4.10.1185")]
-[assembly: AssemblyFileVersion("2018.4.10.1185")]
+[assembly: AssemblyVersion("2018.4.10.1187")]
+[assembly: AssemblyFileVersion("2018.4.10.1187")]
