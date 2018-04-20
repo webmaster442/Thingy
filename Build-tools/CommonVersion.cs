e882df71b772b280386a9c65eb6ada730aabf9d9
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-20 17:40:03 +0200

    Replaced System.Windows.Interactivity.dll nuget source Closes issue #27

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.4.19.1365")]
-[assembly: AssemblyFileVersion("2018.4.19.1365")]
+[assembly: AssemblyVersion("2018.4.20.1367")]
+[assembly: AssemblyFileVersion("2018.4.20.1367")]
