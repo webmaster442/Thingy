4d60c9eb9754ad722b4185edb7bb10f0f52c9123
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-20 18:35:59 +0200

    Moved tests solution

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.4.20.1377")]
-[assembly: AssemblyFileVersion("2018.4.20.1377")]
+[assembly: AssemblyVersion("2018.4.20.1378")]
+[assembly: AssemblyFileVersion("2018.4.20.1378")]
