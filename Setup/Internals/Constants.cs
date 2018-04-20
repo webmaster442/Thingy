e50b3c076070c99be4022d8683422ac8eed61dcb
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-20 20:29:58 +0200

    Basic setup

---
+++ Setup/Internals/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Setup.Internals
+{
+    internal static class Constants
+    {
+        public const string InstallDir = @"%AppDataFolder%\Webmaster442\Thingy";
+        public static readonly Guid InstallerGUID = new Guid("6fe30b47-2577-43ad-9085-1861ba25849b");
+        public static readonly Guid BootstrapGUID = new Guid("2B1CEB2C-4382-4ABC-9D65-4CCCB120BE93");
+        public const string OutputDirectory = @"..\bin\Installer";
+        public const string LicenceFile = @"gpl-3.0.rtf";
+        public const string FrameworkDloadUrl = @"https://download.microsoft.com/download/8/E/2/8E2BDDE7-F06E-44CC-A145-56C6B9BBE5DD/NDP471-KB4033344-Web.exe";
+        public const string FrameworkInstallerFile = @"Net471-web.exe";
+        public const string InstallerName = @"thingy-setup";
+
+        public const string Banner = "banner.bmp";
+    }
+}
---
+++ Setup/Program.cs
@@ -0,0 +1,47 @@
+﻿using Setup.Internals;
+using WixSharp;
+using WixSharp.Forms;
+
+namespace Setup
+{
+    class Program
+    {
+        static void Main()
+        {
+            var project = new ManagedProject("Thingy")
+            {
+                InstallPrivileges = InstallPrivileges.limited,
+                OutDir = Constants.OutputDirectory,
+                LicenceFile = Constants.LicenceFile,
+                Platform = Platform.x64,
+                Version = InstallerHelper.GetThingyVersion(),
+                GUID = Constants.InstallerGUID,
+                ManagedUI = ManagedUI.Empty,
+                Dirs = new Dir[]
+                {
+                    new Dir(Constants.InstallDir,
+                           InstallerHelper.GetFiles(InstallerHelper.ThingyReleaseDirectory,".dll", ".exe", ".config", ".vbs"))
+                }
+            };
+            project.BackgroundImage = Constants.Banner;
+            project.ManagedUI = ManagedUI.Default;
+
+            //custom set of standard UI dialogs
+            project.ManagedUI = new ManagedUI();
+
+            project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
+                                            .Add(Dialogs.Licence)
+                                            .Add(Dialogs.SetupType)
+                                            .Add(Dialogs.InstallDir)
+                                            .Add(Dialogs.Progress)
+                                            .Add(Dialogs.Exit);
+
+            project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
+                                           .Add(Dialogs.Progress)
+                                           .Add(Dialogs.Exit);
+
+            Compiler.AllowNonRtfLicense = true;
+            Compiler.BuildMsi(project);
+        }
+    }
+}
