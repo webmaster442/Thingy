90a11f3e0dbb378684a9edb6b9dff730a9a34e80
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-04-22 17:30:00 +0200

    Youtube-dl frontend

---
+++ Modules/Thingy.Mpv/ModuleDefinitions/YoutubeDlModule.cs
@@ -0,0 +1,51 @@
+﻿using AppLib.WPF;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Thingy.API;
+
+namespace Thingy.Mpv.ModuleDefinitions
+{
+    public class YoutubeDlModule : ModuleBase
+    {
+        public override string ModuleName
+        {
+            get { return "YoutubeDl"; }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return BitmapHelper.FrozenBitmap("pack://application:,,,/Thingy.Resources;component/Icons/icons8-youtube-96.png"); }
+        }
+
+        public override string Category
+        {
+            get { return ModuleCategories.Utilities; }
+        }
+
+        public override UserControl RunModule()
+        {
+            return new Views.YoutubeDlView(App);
+        }
+
+        public override bool CanLoad
+        {
+            get
+            {
+                var youtubedl = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Apps\x64\youtube-dl.exe");
+                return System.IO.File.Exists(youtubedl);
+            }
+        }
+
+        public override OpenParameters OpenParameters
+        {
+            get
+            {
+                return new OpenParameters
+                {
+                    DialogButtons = DialogButtons.OkCancel
+                };
+            }
+        }
+    }
+}
---
+++ Modules/Thingy.Mpv/Views/YoutubeDlView.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Thingy.API;
+using Thingy.API.Capabilities;
+
+namespace Thingy.Mpv.Views
+{
+    /// <summary>
+    /// Interaction logic for YoutubeDlView.xaml
+    /// </summary>
+    public partial class YoutubeDlView : UserControl, IHaveCloseTask
+    {
+        private IApplication _app;
+
+        public YoutubeDlView(IApplication app)
+        {
+            _app = app;
+            InitializeComponent();
+        }
+
+        private void RunYoutubeDl(string dir, string args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Apps\x64\youtube-dl.exe");
+            if (string.IsNullOrEmpty(dir))
+                p.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            else
+                p.StartInfo.WorkingDirectory = dir;
+
+            p.StartInfo.Arguments = args;
+
+            p.Start();
+        }
+
+        private async Task<string> RunCommand(string dir, string args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Apps\x64\youtube-dl.exe");
+            if (string.IsNullOrEmpty(dir))
+                p.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            else
+                p.StartInfo.WorkingDirectory = dir;
+
+            p.StartInfo.Arguments = args;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+
+            p.Start();
+
+            string result = await p.StandardOutput.ReadToEndAsync();
+            await Task.Run(() => p.WaitForExit());
+
+            return result;
+        }
+
+        private async void BtnFormats_Click(object sender, RoutedEventArgs e)
+        {
+            var o = await RunCommand(null, $"-F {TbUrl.Text}");
+            await _app.ShowMessageBox("Output", o, DialogButtons.Ok);
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            RunYoutubeDl(null, "-U");
+        }
+
+        private void Download()
+        {
+            if (string.IsNullOrEmpty(TbArguments.Text))
+                RunYoutubeDl(TbTargetFolder.Text, $"{TbUrl.Text}");
+            else
+                RunYoutubeDl(TbTargetFolder.Text, $"{TbArguments.Text} {TbUrl.Text}");
+        }
+
+        public Action ClosingTask
+        {
+            get { return Download; }
+        }
+
+        public bool CanExecuteAsync
+        {
+            get { return false; }
+        }
+    }
+}
