f9a88e6e9e7b34ea07e13ffc48782d006da51983
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-05-02 19:13:41 +0200

    AssemblyInfo tweaks

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.5.2.1551")]
-[assembly: AssemblyFileVersion("2018.5.2.1551")]
+[assembly: AssemblyVersion("2018.5.2.1552")]
+[assembly: AssemblyFileVersion("2018.5.2.1552")]
--- Modules/Thingy.MusicPlayer/Properties/AssemblyInfo.cs
+++ Modules/Thingy.MusicPlayer/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -21,6 +22,15 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("253234e3-85e1-4479-813c-96360a670d86")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
 
 // Version information for an assembly consists of the following four values:
 //
