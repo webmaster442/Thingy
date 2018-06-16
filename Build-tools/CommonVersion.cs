09c42bc83ac76316e781ec86e24bfb99ebba266e
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-06-16 20:03:37 +0200

    Linux subsystem module

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.6.15.1691")]
-[assembly: AssemblyFileVersion("2018.6.15.1691")]
+[assembly: AssemblyVersion("2018.6.16.1695")]
+[assembly: AssemblyFileVersion("2018.6.16.1695")]
---
+++ Modules/Thingy.CoreModules/ModuleDefinitons/LinuxSubSystemModule.cs
@@ -0,0 +1,45 @@
+﻿using AppLib.WPF;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Thingy.API;
+
+namespace Thingy.CoreModules.ModuleDefinitons
+{
+    public class LinuxSubSystemModule: ModuleBase
+    {
+        private const string path = @"%windir%\system32\bash.exe";
+
+        public override string ModuleName
+        {
+            get { return "Linux Subsystem for Windows"; }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return BitmapHelper.FrozenBitmap("pack://application:,,,/Thingy.Resources;component/Icons/icons8-linux-96.png"); }
+        }
+
+        public override string Category
+        {
+            get { return ModuleCategories.CommandLine; }
+        }
+
+        public override bool CanLoad
+        {
+            get
+            {
+                return System.IO.File.Exists(Environment.ExpandEnvironmentVariables(path));
+            }
+        }
+
+        public override UserControl RunModule()
+        {
+            var p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = Environment.ExpandEnvironmentVariables(path);
+            p.StartInfo.WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            p.Start();
+            return null;
+        }
+    }
+}
