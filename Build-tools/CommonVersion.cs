e43d2023196a6701a309a9dd44f62e86ea35b467
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-06-29 18:05:27 +0200

    File browser moved to be an internal module

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.6.29.1814")]
-[assembly: AssemblyFileVersion("2018.6.29.1814")]
+[assembly: AssemblyVersion("2018.6.29.1815")]
+[assembly: AssemblyFileVersion("2018.6.29.1815")]
