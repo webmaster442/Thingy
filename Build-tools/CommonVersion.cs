3dbcaf4be5c361854175508def3bd3ac62cf4fc5
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-07-16 20:03:10 +0200

    dvd preset added

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.7.16.2008")]
-[assembly: AssemblyFileVersion("2018.7.16.2008")]
+[assembly: AssemblyVersion("2018.7.16.2010")]
+[assembly: AssemblyFileVersion("2018.7.16.2010")]
