5b701d58a8b1b6a203903ea811609659859c9caa
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-07-16 19:29:42 +0200

    FFMpeg now handles files

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.7.16.2007")]
-[assembly: AssemblyFileVersion("2018.7.16.2007")]
+[assembly: AssemblyVersion("2018.7.16.2008")]
+[assembly: AssemblyFileVersion("2018.7.16.2008")]
