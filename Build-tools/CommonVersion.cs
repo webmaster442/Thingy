18b3ef35409bd3d6a2348965f894cb978957cbdf
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-07-23 19:14:55 +0200

    Bang search improvements

--- Build-tools/CommonVersion.cs
+++ Build-tools/CommonVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2018.7.23.2028")]
-[assembly: AssemblyFileVersion("2018.7.23.2028")]
+[assembly: AssemblyVersion("2018.7.23.2032")]
+[assembly: AssemblyFileVersion("2018.7.23.2032")]
