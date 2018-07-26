b080618e62616823d429b026df1b42734284a079
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-07-26 20:25:47 +0200

    Web browser locator code

---
+++ Thingy/InternalCode/WebBroswserLocator.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Thingy.InternalCode
+{
+    internal static class WebBroswserLocator
+    {
+        private static IList<WebBrowser> GetBrowsers()
+        {
+            var browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Clients\StartMenuInternet");
+            var ret = new List<WebBrowser>();
+
+            if (browserKeys == null)
+            {
+                browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Clients\StartMenuInternet");
+            }
+
+            var browserNames = browserKeys.GetSubKeyNames();
+
+            foreach (var browserName in browserNames)
+            {
+                var browserKey = browserKeys.OpenSubKey(browserName);
+                var name = browserKey.GetValue(null).ToString();
+
+                RegistryKey registryKey = browserKey.OpenSubKey(@"shell\open\command");
+                var path = registryKey.GetValue(null).ToString().StripQuotes();
+
+                var version = "unknown";
+                if (!string.IsNullOrEmpty(path))
+                {
+                    version = FileVersionInfo.GetVersionInfo(path).FileVersion;
+                }
+
+                RegistryKey browserIconPath = browserKey.OpenSubKey(@"DefaultIcon");
+                var icon = browserIconPath.GetValue(null).ToString().StripQuotes();
+
+                ret.Add(new WebBrowser(name, path, version, icon));
+            }
+
+            WebBrowser edgeBrowser = GetEdgeVersion();
+            if (edgeBrowser != null)
+            {
+                ret.Add(edgeBrowser);
+            }
+
+            return ret;
+        }
+
+        private const string registryEdge = @"SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\Schemas";
+
+        private static WebBrowser GetEdgeVersion()
+        {
+            RegistryKey edgeKey = Registry.CurrentUser.OpenSubKey(registryEdge);
+            if (edgeKey != null)
+            {
+                string version = edgeKey.GetValue("PackageFullName").ToString().StripQuotes();
+                Match result = Regex.Match(version, "(((([0-9.])\\d)+){1})");
+                if (result.Success)
+                {
+                    return new WebBrowser("Edge", "start microsoft-edge:", result.Value, null);
+                }
+            }
+            return null;
+        }
+
+        internal static String StripQuotes(this String s)
+        {
+            if (s.EndsWith("\"") && s.StartsWith("\""))
+                return s.Substring(1, s.Length - 2);
+            else
+                return s;
+        }
+    }
+}
---
+++ Thingy/InternalCode/WebBrowser.cs
@@ -0,0 +1,18 @@
+﻿namespace Thingy.InternalCode
+{
+    internal class WebBrowser
+    {
+        public string Name { get; }
+        public string Path { get; }
+        public string IconPath { get; }
+        public string Version { get; }
+
+        public WebBrowser(string name, string path, string version, string icon)
+        {
+            Name = name;
+            Path = path;
+            Version = version;
+            IconPath = icon;
+        }
+    }
+}
