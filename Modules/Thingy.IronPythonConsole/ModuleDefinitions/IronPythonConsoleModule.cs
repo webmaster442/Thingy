4f094064798007fae07adac7fbc2386f480a4c47
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-08-02 20:57:59 +0200

    Added module definiton and Main module view

---
+++ Modules/Thingy.IronPythonConsole/ModuleDefinitions/IronPythonConsoleModule.cs
@@ -0,0 +1,30 @@
+﻿using AppLib.WPF;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Thingy.API;
+
+namespace Thingy.IronPythonConsole.ModuleDefinitions
+{
+    public class IronPythonConsoleModule : ModuleBase
+    {
+        public override string ModuleName
+        {
+            get { return "IronPython"; }
+        }
+
+        public override ImageSource Icon
+        {
+            get { return BitmapHelper.FrozenBitmap("pack://application:,,,/Thingy.Resources;component/Icons/icons8-python-96.png"); }
+        }
+
+        public override string Category
+        {
+            get { return ModuleCategories.CommandLine; }
+        }
+
+        public override UserControl RunModule()
+        {
+            return new Views.IronPythonConsole(App);
+        }
+    }
+}
---
+++ Modules/Thingy.IronPythonConsole/Views/IronPythonConsole.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Thingy.API;
+
+namespace Thingy.IronPythonConsole.Views
+{
+    /// <summary>
+    /// Interaction logic for IronPythonConsole.xaml
+    /// </summary>
+    public partial class IronPythonConsole : UserControl
+    {
+        private readonly IApplication _app;
+
+        public IronPythonConsole()
+        {
+            InitializeComponent();
+        }
+
+        public IronPythonConsole(IApplication app): this()
+        {
+            _app = app;
+        }
+    }
+}
